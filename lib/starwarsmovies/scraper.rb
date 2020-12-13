cb29cfccd867b26ed4a61ac69e9332e05918f88f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-12-13 12:48:27 -0500

    Updated scraper file

--- lib/starwarsmovies/scraper.rb
+++ lib/starwarsmovies/scraper.rb
@@ -1,13 +1,13 @@
 class Starwarsmovies::Scraper
 
-    def self.scrape_movies
-      url = "https://starwarscanontimeline.com/movie/"
-      doc = Nokogiri::HTML(open(url))
-      movies = doc.css("#entry-header header")
-      movies.each do |d|
-          ref = d.css("a").attr("href").text.strip
-          name = d.css("h3.entry-title").text.strip
-          Starwarsmovies::Movies.new(name, ref)
-      end
+  def self.scrape_movies
+    url = "https://starwarscanontimeline.com/movie/"
+    doc = Nokogiri::HTML(open(url))
+    movies = doc.css("div.card-content")
+    movies.each do |d|
+        ref = d.css("a").attr("href").text.strip
+        name = d.css("h3.entry-title.title.is-uppercase").text.strip
+        Starwarsmovies::Movies.new(name, ref)
     end
   end
+end
